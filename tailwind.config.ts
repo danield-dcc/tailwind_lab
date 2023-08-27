5aacb264fb8fab9e0aa7bd3d9d9ebc815c713919
Daniel <danieldcc.colares@gmail.com>
2023-08-27 12:25:41 -0300

    💫 Adds select/files animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,21 @@ const config: Config = {
       maxWidth: {
         app: '700px',
       },
+
+      keyframes: {
+        sileDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        // slideUpAndFade: {
+        //   from: { opacity: '1' },
+        //   to: { opacity: '0' },
+        // },
+      },
+      animation: {
+        sileDownAndFade: 'sileDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        // slideUpAndFade: 'slideUpAndFade 1s linear',
+      },
     },
   },
   plugins: [],
