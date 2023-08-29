b9a6a7380b399dad148374eb24b13e57f4b0b3d4
Daniel <danieldcc.colares@gmail.com>
2023-08-29 19:04:38 -0300

    🎨 dark theme on sidebar

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 type InputPrefixProps = ComponentProps<'div'>
 
@@ -11,7 +12,10 @@ type InputControl = ComponentProps<'input'>
 export function Control(props: InputControl) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 outline-none text-zinc-900 placeholder-zinc-600"
+      className={twMerge(
+        'flex-1 border-0 bg-transparent p-0 outline-none text-zinc-900 placeholder-zinc-600 ',
+        'dark:text-zinc-100 dark:placeholder-zinc-400',
+      )}
       placeholder="Search"
       {...props}
     />
@@ -23,8 +27,13 @@ export type InputRootProps = ComponentProps<'div'>
 export function Root(props: InputRootProps) {
   return (
     <div
-      className="flex  w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
+      className={twMerge(
+        'flex  w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm ',
+        'focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100',
+        'dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/20',
+        props.className,
+      )}
       {...props}
-    ></div>
+    />
   )
 }
--- src/components/Siderbar/UserProfile.tsx
+++ src/components/Siderbar/UserProfile.tsx
@@ -11,10 +11,12 @@ export function UserProfile() {
         alt=""
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Daniel Conceição
         </span>
-        <span className="truncate text-sm text-zinc-500">daniel@gmail.com</span>
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
+          daniel@gmail.com
+        </span>
       </div>
       <Button type="button" variant="ghost">
         <LogOut className="w-5 h-5 text-zinc-500" />
